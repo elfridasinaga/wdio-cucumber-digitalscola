dc0b6f165ce0ce4a40b1b4a71625e1e276f9c763
elfridals12@gmail.com <63908025+elfridasinaga@users.noreply.github.com>
2024-05-05 23:05:04 +0700

    cucumber tes

---
+++ features/pageobjects/sauce-login.page.js
@@ -0,0 +1,42 @@
+const { browser, $ } = require('@wdio/globals')
+
+class SauceLoginPage {
+
+    get username() {
+        return $('#user-name');
+    }
+
+    get password() {
+        return $('#password');
+    }
+
+    get loginButton() {
+        return $('#login-button');
+    }
+
+    async inputUsername(username) {
+        await this.username.setValue(username)
+    }
+
+    async inputPassword(password) {
+        await this.password.setValue(password)
+    }
+
+    async clickLoginButton() {
+        await this.loginButton.click()
+    }
+
+    async login(username, password) {
+        await this.inputUsername(username)
+        await this.inputPassword(password)
+        await this.clickLoginButton()
+        await browser.pause(5000)
+    }
+
+    open() {
+        return browser.url(`https://www.saucedemo.com/`);
+    }
+    
+}
+
+module.exports = new SauceLoginPage();
---
+++ features/step-definitions/sauce-steps.js
@@ -0,0 +1,38 @@
+const { Given, When, Then } = require('@wdio/cucumber-framework');
+const { browser } = require('@wdio/globals')
+
+const SauceLoginPage = require('../pageobjects/sauce-login.page.js');
+const DashboardPage = require('../pageobjects/sauce-dashboard.js');
+
+Given("user is on sauce-demo page", async () => {
+    await SauceLoginPage.open()
+})
+
+When(/^user input username with "(.*)"$/, async (username) => {
+    await SauceLoginPage.inputUsername(username)
+})
+
+When(/^user input password with "(.*)"$/, async (password) => {
+    await SauceLoginPage.inputPassword(password)
+  });
+  
+When(/^user click button$/, async () => {
+    await SauceLoginPage.clickLoginButton()
+});
+
+Then(/^user should redirect to homepage$/, async () => {
+    await DashboardPage.validateOnPage()
+});
+
+Then(/^user login using username "(.*)" and password "(.*)"$/, async (username, password) => {
+    await SauceLoginPage.login(username, password)
+});
+
+Then(/^user click cart button"$/, async (username, password) => {
+    await SauceLoginPage.login(username, password)
+});
+
+Then(/^user click cart button"$/, async (username, password) => {
+    await SauceLoginPage.login(username, password)
+});
+
